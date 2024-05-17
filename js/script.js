64f7f22b7e94d2531187c67cb64791ab2175544e
Il tuo nome <la_tua_email@example.com>
2024-05-17 20:31:46 +0200

    setting up html

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// const gridElement = document.querySelector ("section#grid")
+
+// const squareElment = document.createElement("article")
+
+//     for (let index = 0; index < 64; index++) {
+//         const element = array[index];
+//         const squareElment = document.createElement("article");
+//         squareElment.classList.add("square");
+//         gridElement.appendChild(squareElment);
+//     }
