bbffd2dfdcb48bc71260f9eba20062b579c751f1
Il tuo nome <la_tua_email@example.com>
2024-05-18 16:14:06 +0200

    trying to make button work

--- js/script.js
+++ js/script.js
@@ -2,24 +2,31 @@ const gridElement = document.querySelector("#myGrid")
 
 const squareElment = document.createElement("div")
 
-    for (let index = 1; index <= 100; index++) {
-        
-        const squareElment = document.createElement("div");
-        squareElment.classList.add("quadratino");
+const myButton = document.querySelector("body > header > nav > div > div > div.pe-3 > button")
 
+myButton.addEventListener("click", function(gridGenerator));
+    
+    function gridGenerator() {
+        for (let index = 1; index <= 100; index++) {
         
-        const contentEl = document.createElement("span");
-        contentEl.classList.add("content");
-        contentEl.append(index);
-        squareElment.appendChild(contentEl);
-
-        
-        squareElment.addEventListener("click", function () {
-            squareElment.classList.add("active")
-            console.log(index)
-            contentEl.classList.add("active");
-        }); 
-
-        
-        gridElement.appendChild(squareElment);
+            const squareElment = document.createElement("div");
+            squareElment.classList.add("quadratino");
+    
+            
+            const contentEl = document.createElement("span");
+            contentEl.classList.add("content");
+            contentEl.append(index);
+            squareElment.appendChild(contentEl);
+    
+            
+            squareElment.addEventListener("click", function () {
+                squareElment.classList.add("active")
+                console.log(index)
+                contentEl.classList.add("active");
+            }); 
+    
+            
+            gridElement.appendChild(squareElment);
+        }
+    
     }
